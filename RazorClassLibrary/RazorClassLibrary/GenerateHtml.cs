410509809992d719be7a372825f58c92382666a3
HowardRichards <dev@conficient.com>
2013-11-27 15:08:30 +0000

    Added code to fetch resource string

--- RazorClassLibrary/RazorClassLibrary/GenerateHtml.cs
+++ RazorClassLibrary/RazorClassLibrary/GenerateHtml.cs
@@ -8,14 +8,35 @@ namespace RazorClassLibrary
 {
     public static class GenerateHtml
     {
+        /// <summary>
+        /// Generate order Html
+        /// </summary>
+        /// <returns></returns>
         public static string ForOrder()
         {
             var order = new OrderModel();
-            // TODO: C# class library does not support razor, so
-            // amend it so it does and add RazorEngine so we can create Html content
-            // using a Razor .cshtml page resource
+            var template = GetTemplate("RazorClassLibrary.OrderTemplate.cshtml");
+
+            // TODO: add RazorEngine to support rendering
 
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Read an embedded resource as a string
+        /// </summary>
+        private static string GetTemplate(string resourceName)
+        {
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+            using (var stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                using (var reader = new System.IO.StreamReader(stream))
+                {
+                    string result = reader.ReadToEnd();
+                    return result;
+                }
+            }
         }
     }
 }
